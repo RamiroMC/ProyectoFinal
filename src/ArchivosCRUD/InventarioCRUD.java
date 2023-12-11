5583353672fab236f0dad7a7c49fe4b8c077495c
Mondongo <Mondongo@DESKTOP-6DPPFHC>
2023-12-11 16:24:28 -0500

    yo

---
+++ src/ArchivosCRUD/InventarioCRUD.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ArchivosCRUD;
+
+/**
+ *
+ * @author JOSE
+ */
+public class InventarioCRUD {
+    
+     private static final String URL = "..........";
+    
+    public static void Create(){
+        
+    }
+    
+    public static void Reed(){
+        
+    }
+    
+    public static void Update(){
+        
+    }
+    
+    public static void Delete(){
+        
+    }
+    
+}
